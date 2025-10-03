ca89796ca31d1fbbcd76f9a07535a046db5c7020
Numan <mahmud18379@gmail.com>
2025-10-03 09:23:25 +0600

    second commit

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,31 +4,15 @@ import database from "@/lib/MongoDB";
 import { Types } from "mongoose";
 import type { MetadataRoute } from "next";
 
-const CHUNK_SIZE = 50000; // Google limit
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-export async function generateSitemaps() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   await database();
-  const totalBlogs = await blogModel.countDocuments();
-  const chunks = Math.ceil(totalBlogs / CHUNK_SIZE);
-  return Array.from({ length: chunks }, (_, i) => ({ id: i }));
-}
-
-export default async function sitemap({
-  id,
-}: {
-  id: number;
-}): Promise<MetadataRoute.Sitemap> {
-  await database();
-  const skip = id * CHUNK_SIZE;
-
-  const allBlogs = await blogModel
+  const blogs = await blogModel
     .find({}, { _id: 1, createdAt: 1 })
-    .skip(skip)
-    .limit(CHUNK_SIZE)
     .lean<{ _id: Types.ObjectId; createdAt: Date }[]>();
 
-  return allBlogs.map((blog) => ({
+  return blogs.map((blog) => ({
     url: `${BASE_URL}/single/${blog._id.toString()}`,
     lastModified: blog.createdAt,
   }));
